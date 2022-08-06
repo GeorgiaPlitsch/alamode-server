ea058026b8291c2bf12936e469cfe5f18aab9d28
Georgia Plitsch <georgia.plitsch@outlook.com>
2022-08-06 13:23:46 +0100

    request created to get reccomendation quiz questions seperately to scored questions

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,7 +1,7 @@
 const quizRouter = require("express").Router();
 const quizController = require("../controllers/quizController");
 
-quizRouter.get("/", quizController.getScoredQuestions);
+quizRouter.get("/scored", quizController.getScoredQuestions);
 quizRouter.get("/rec", quizController.getRecQuestions);
 
 module.exports = quizRouter;
